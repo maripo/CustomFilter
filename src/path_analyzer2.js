1cbf98b0f8a7bec3eb4a456bf048a78d8f21d995
Maripo GODA <avenafatua@gmail.com>
2011-09-05 21:22:05 +0900

    XPath suggestion algorithm

--- src/path_analyzer2.js
+++ src/path_analyzer2.js
@@ -42,6 +42,42 @@ PathAnalyzer.prototype.createPathList = function ()
 	 * tagName (必須)
 	 * id (自分自身にidがついてる場合は必ず出す。絞り込む場合は、その上位のidをいくつか使う)
 	 */
+	this.pathList = new Array();
+	{
+		var testNode = this.ancestors[0];
+		var tagName = testNode.node.tagName;
+		if (testNode.classes.length>1)
+		{
+			for (var i=0, l=testNode.classes.length; i<l; i++)
+			{
+				var xpath = '//'
+					+tagName
+					+'[contains(concat(" ",normalize-space(@class)," "),"'
+					+testNode.classes[i]
+					+'")]';
+				this.pathList.push(new PathFilter(xpath));
+			}
+		}
+		else if (testNode.classes.length==1)
+		{
+			var className = testNode.classes[0];
+			this.pathList.push(new PathFilter('//'+testNode.node.tagName+'[@class="'+className+'"]'));
+
+			var xpath = '//'
+				+tagName
+				+'[contains(concat(" ",normalize-space(@class)," "),"'
+				+testNode.classes[0]
+				+'")]';
+			this.pathList.push(new PathFilter(xpath));
+		}
+		else
+		{
+			this.pathList.push(new PathFilter("//"+testNode.node.tagName));
+		}
+	}
+	/*
+	 * そもそも、「クラスあり」と「クラスなし」で明確にわけたほうがいいんじゃなかろーか?
+	 */
 	return this.pathList;
 };
 
@@ -111,177 +147,17 @@ var PathEdge = function (elements)
 
 /**
  * PathElement
- * @param {Object} node
  */
+var splitSpacesRegExp = new RegExp(' +', 'g');
 var PathElement = function (node) 
 {
 	this.node = node;
 	this.isTarget = false;
 	this.parentNode = null;
 	this.upperKeyNode = null;
+	this.classes = (null==node.className||''==node.className)?new Array():node.className.replace(splitSpacesRegExp, ' ').split(' ');
 }
-PathElement.arrayContains = function (array, str) 
-{
-	for (var i=0, l=array.length; i<l; i++) if (str==array[i].xpath) return true;
-	return false;
-}
-PathElement.prototype.countSimilarChildNodes = function (child, rootNode) 
-{
-	this.similarChildNodesCount = 0;
-	if (!child) return;
-	var childMatchCount = child.node.getElementsByTagName(rootNode.tagName).length;
-	var selfMatchCount = this.node.getElementsByTagName(rootNode.tagName).length;
-	if (childMatchCount==1 || childMatchCount==selfMatchCount) return;
-	var tagName = child.node.tagName;
-	var count = 0;
-	var children = this.node.childNodes;
-	for (var i=0, l=children.length; i<l; i++) 
-	{
-		if (tagName == children[i].tagName) 
-		{
-			if (children[i]==child.node) 
-			{
-				this.childIndex = count;
-			}
-			count++;
-		}
-	}
-	this.childTagName = child.node.tagName;
-	this.similarChildNodesCount = count;
-}
-var PartialFilter = function (targetNode, edge, branchNode,index, isMulti, allowId) 
-{
-		this.targetNode = targetNode;
-		this.edge = edge;
-		this.branchNode = branchNode;
-		this.index = index;
-		this.isMulti = isMulti;
-		this.conditions = new Array();
-		this.allowId = allowId;
-};
-PartialFilter.prototype.getDefId = function () 
-{
-	if (!isEmpty(this.targetNode.id)) return this.targetNode.id;
-	for (var i=0, l=this.edge.elements.length; i<l; i++) 
-	{
-		if (!isEmpty(this.edge.elements[i].node.id))
-			return this.edge.elements[i].node.id;
-	}
-	return null;
-}
-
-PartialFilter.prototype.findValidConditions = function (childFilter) 
-{
-	if (this.isMulti) this.findValidConditionsMulti(childFilter);
-	else this.findValidConditionsUnique(childFilter);
-};
-
-PartialFilter.prototype.compareValidConditionsMulti = function (childFilter, xpath) 
-{
-	var addCount = 0;
-	if (childFilter) 
-	{
-		for (var i=0, l=childFilter.conditions.length; i<l; i++) 
-		{
-			var condition = childFilter.conditions[i];
-			var fullXPath = xpath + condition.getFullXPath();
-			var hit = getRelativeElementsByXPath(this.branchNode.element.node.parentNode||this.branchNode.element.node, fullXPath);
-			if (hit.length > condition.hitNodes.length) 
-			{
-				this.conditions.push(new FilterCondition(xpath, hit, condition));
-				addCount ++;
-			} 
-		}
-	}
-	else 
-	{
-		var hit = getRelativeElementsByXPath(this.branchNode.element.node.parentNode||this.branchNode.element.node, xpath);
-		if (hit.length>1) 
-		{
-			this.conditions.push(new FilterCondition(xpath, hit));
-			addCount ++;
-		}
-	}
-	return addCount;
-}
-PartialFilter.prototype.findValidConditionsMulti = function (childFilter) 
-{
-	var tagName = this.targetNode.element.node.tagName;
-	{
-		var xpath = '//' + tagName;
-		if (childFilter) 
-		{
-			this.compareValidConditionsMulti(childFilter, xpath);
-		} 
-		else 
-		{
-			var hit = getRelativeElementsByXPath(this.branchNode.element.node.parentNode||this.branchNode.element.node, xpath);
-			if (hit.length>1) 
-			{
-				this.conditions.push(new FilterCondition(xpath, hit));
-			}
-		}
-	}
-	{
-		if (childFilter) 
-		{
-			var xpath = '';
-			this.compareValidConditionsMulti(childFilter, xpath);
-		}
-	}
-	//class名による検索
-	var classNames = this._getClassNames(this.targetNode.element.node);
-	for (var i=0, l=classNames.length; i<l; i++) 
-	{
-		var xpath = '//'
-			+tagName
-			+'[contains(concat(" ",normalize-space(@class)," "),"'
-			+classNames[i]
-			+'")]';
-		
-		if (childFilter) 
-		{
-			this.compareValidConditionsMulti(childFilter, xpath);
-		}
-		else 
-		{
-			var hit = getRelativeElementsByXPath(this.branchNode.element.node.parentNode||this.branchNode.element.node, xpath);
-			if (hit.length > 1) 
-			{
-				this.conditions.push(new FilterCondition(xpath, hit));
-			}
-		}
-	}
-};
-
-PartialFilter.prototype.compareValidConditionsUnique = function (childFilter, xpath) 
-{
-	var addCount = 0;
-	if (childFilter) 
-	{
-		for (var i=0, l=childFilter.conditions.length; i<l; i++) 
-		{
-			var filter = new FilterCondition(xpath,childFilter.conditions[i].hitNodes, childFilter.conditions[i]);
-			var condition = childFilter.conditions[i];
-			var fullXPath = xpath + condition.getFullXPath();
-			var hit = getRelativeElementsByXPath(this.branchNode.element.node.parentNode||this.branchNode.element.node, fullXPath);
-			if (hit.length == condition.hitNodes.length) 
-			{
-				this.conditions.push(new FilterCondition(xpath, hit, condition));
-				addCount ++;
-			} 
-		}
-	} 
-	else 
-	{
-		var hit = getRelativeElementsByXPath(this.branchNode.element.node.parentNode||this.branchNode.element.node, xpath);
-		if (hit.length > 0) 
-		{
-			this.conditions.push(new FilterCondition(xpath, [this.targetNode.element.node]));
-		}
-	}
-	return addCount;
-};
+// 参考としてとっておくが、あとでけす。
 PartialFilter.prototype.findValidConditionsUnique = function (childFilter) 
 {
 	//TODO 「結果的にユニークになる」条件はもっといろいろあるはず (特に、子フィルタがclass名などでフィルタしてると、ユニークに決まることもある)
@@ -332,94 +208,22 @@ PartialFilter.prototype.findValidConditionsUnique = function (childFilter)
 		}
 	}
 };
-PartialFilter.prototype.getElementsLinearList = function () 
-{
-	var list = new Array();
-	list.push(this.targetNode.element);
-	for (var i=0, l=this.edge.elements.length; i<l; i++) 
-	{
-		list.push(this.edge.elements[i]);
-	}
-	return list;
-};
-var splitSpacesRegExp = new RegExp(' +', 'g');
-PartialFilter.prototype._getClassNames = function (node) 
-{
-	var str = node.className;
-	if (isEmpty(str)) return new Array();
-	return str.replace(splitSpacesRegExp, ' ').split(' ');
-};
+
 function isEmpty (str) 
 {
 	return (null==str || ''==str);
 };
-var Filter = function (boolArray, sequence) 
-{
-	this.sequence = sequence;
-	this.partialFilters = new Array();
-	//TODO 最後の "false" だった場合は、それより上はpushしない
-	var lastUniqueIndex = boolArray.length;
-	
-	for (var i=boolArray.length-1; i>=0; i--) 
-	{
-		if (false == boolArray[i]) 
-		{
-			lastUniqueIndex = i+1;
-		} 
-		else 
-		{
-			break;
-		}
-	}
-	
-	for (var i=0; i<lastUniqueIndex; i++) 
-	{
-		var partial = new PartialFilter(
-			this.sequence[i*2],
-			this.sequence[i*2+1],
-			this.sequence[i*2+2],
-			i,
-			boolArray[i],
-			(i==lastUniqueIndex-1 && !boolArray[i])
-		);
-		this.partialFilters.push(partial);
-	}
-};
-Filter.prototype.analyze = function (list) 
-{
-	//console.log("Filter.analyze length=" + this.partialFilters.length);
-	for (var i=0, l=this.partialFilters.length; i<l;i++) 
-	{
-		var partial = this.partialFilters[i];
-		var childFilter = (i>0)?this.partialFilters[i-1]:null;
-		partial.findValidConditions(childFilter);
-	}
-	var topFilter = this.partialFilters[this.partialFilters.length-1];
-	for (var i=0, l=topFilter.conditions.length; i<l; i++) 
-	{
-		list.push(new PathFilter(topFilter.conditions[i].getFullXPath()))
-	}
-};
-var FilterCondition = function (xpath, hitNodes, childFilter) 
-{
-	this.xpath = xpath;
-	this.hitNodes = hitNodes;
-	this.childFilter = childFilter;
-};
-FilterCondition.prototype.getFullXPath = function () 
-{
-	if (this.childFilter) 
-	{
-		return this.xpath + this.childFilter.getFullXPath();
-	}
-	else 
-		return this.xpath;
-}
 // TODO フルパスを表示できるようにすべき 
 // TODO Utilに引っ越すべき
 PathElement.notEmpty = function (str) 
 {
 	return (str && ''!=str);
+}
+//TODO Utilに引っ越すべき
+PathElement.arrayContains = function (array, str) 
+{
+	for (var i=0, l=array.length; i<l; i++) if (str==array[i].xpath) return true;
+	return false;
 }
 
 // TODO Utilに引っ越すべき

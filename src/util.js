c42129639e60250c841bcb43f608646be38da6ea
maripo <avenafatua@gmail.com>
2011-09-06 23:40:03 +0900

    adding some funcs

--- src/util.js
+++ src/util.js
@@ -27,3 +27,38 @@ Util.shorten = function (text, limit)
  	if (text.length<limit) return text;
 	return text.substring(0, limit) + '...';
  };
+Util.getRelativeElementsByXPath = function(targetNode, xpath)
+{
+	var list = new Array();
+	try 
+	{
+		var result = document.evaluate(xpath, targetNode, null, XPathResult.ANY_TYPE, null);
+		var node;
+		
+		while (node = result.iterateNext()) 
+		{
+			list.push(node);
+		}
+	} 
+	catch (e) 
+	{
+		console.log(e)
+	}
+	return list;
+};
+Util.arrayContains = function (array, str) 
+{
+	for (var i=0, l=array.length; i<l; i++) if (str==array[i].xpath) return true;
+	return false;
+};
+
+
+Util.isEmpty = function (str) 
+{
+	return (null==str || ''==str);
+};
+
+Util.notEmpty = function (str)
+{
+	return !Util.isEmpty(str);
+}

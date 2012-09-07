2207319424b712cea004d14cd6d4f4b4957c671f
Maripo GODA <avenafatua@gmail.com>
2012-09-07 13:34:30 +0900

    import/exportのローカライズ漏れ修正

--- src/util.js
+++ src/util.js
@@ -163,6 +163,7 @@ CustomBlockerUtil.localize = function ()
 		var element = tags[i];
 		if (null!=element.className && element.className.match(CustomBlockerUtil.LOCALIZE_CLASS_REGEXP))
 		{
+			Log.v("CustomBlockerUtil.localize " + element.innerHTML + "->" + chrome.i18n.getMessage(RegExp.$1));
 			element.innerHTML = chrome.i18n.getMessage(RegExp.$1);
 		}
 	}
@@ -172,6 +173,7 @@ CustomBlockerUtil.localize = function ()
 		if ('button'!=element.type) continue;
 		if (null!=element.className && element.className.match(CustomBlockerUtil.LOCALIZE_CLASS_REGEXP))
 		{
+			Log.v("CustomBlockerUtil.localize " + element.value + "->" + chrome.i18n.getMessage(RegExp.$1));
 			element.value = chrome.i18n.getMessage(RegExp.$1);
 		}
 	}
@@ -379,7 +381,7 @@ Log.Level =
 		WARNING: 4,
 		ERROR: 5
 };
-Log.FILTER_LEVEL = Log.Level.WARNING;
+Log.FILTER_LEVEL = Log.Level.VERBOSE;
 
 Log._write = function (message, level, label)
 {

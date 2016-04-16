b0636006d1d547afe506c8658a195dc0188261ee
Mariko Hisakawa <avenafatua@gmail.com>
2016-04-17 00:41:46 +0900

    Fix bug of URL matching with wildcards
    * Fix "wildcardToRegExp" function
    * URL matchers were not properly replaced.
    * Fix English description below URL matcher textbox. (RegEx -> Expression)

--- src/util.js
+++ src/util.js
@@ -48,7 +48,9 @@ CustomBlockerUtil.getElementsByCssSelector = function (selector)
 var REGEX_WILDCARD_TO_REGEXP = new RegExp('([^A-Za-z0-9_\\*])', 'g');
 var REGEX_WILDCARD = new RegExp('\\*', 'g');
 CustomBlockerUtil.wildcardToRegExp = function (str) {
-	var result = ".*"+str.replace(REGEX_WILDCARD_TO_REGEXP, function(){return '\\' + RegExp.$1}).replace(REGEX_WILDCARD, '.*')+".*";
+	var result = ".*" 
+		+ str.replace(REGEX_WILDCARD_TO_REGEXP, '\\$1').replace(REGEX_WILDCARD, '.*')
+		+ ".*";
 	return result;
 };
 var REGEX_DOUBLE_SLASH = new RegExp('//','g');

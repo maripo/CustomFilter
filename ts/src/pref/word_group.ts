2a70e24e4b31344aa22c78dc244aad73005618e9
Mariko Hisakawa <avenafatua@gmail.com>
2018-08-31 23:34:01 +0900

    Create dummy word groups

--- ts/src/pref/word_group.ts
+++ ts/src/pref/word_group.ts
@@ -1,1 +1,10 @@
-window.onload = CustomBlockerUtil.localize;
+function initWordGroup () {
+	CustomBlockerUtil.localize;
+	console.log("initWordGroup");
+	let groups = [] as [WordGroup];
+	for (let i=0; i<8; i++) {
+		groups.push(cbStorage.createWordGroup());
+	}
+	console.log(groups);
+}
+window.onload = initWordGroup;
